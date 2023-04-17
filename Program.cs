e8f256e13e017c3c8864cb3800db91a56084d26e
DANTE VERDI GUTIERREZ <47573312@ort>
2023-04-17 10:11:33 -0300

    menu

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
 ﻿
+
+bool chau = true;
+
+while (chau) {
+
+    Console.WriteLine("Menu: \n1. Nueva inscripcion\n2. Obtener estadisticas del evento\n3. Buscar cliente\n4. Cambiar entrada de un cliente\n5. Salir");
+    string r=Console.ReadLine();
+    switch(r){
+        case ("1"):
+            
+        break;
+        case ("2"):
+            
+        break;
+        case ("3"):
+            
+        break;
+        case ("4"):
+            
+        break;
+        case ("5"):
+        chau=false;    
+        break;
+        
+        default:
+        break;
+    }
+    
+
+
+}
