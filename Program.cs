9e483f90b9916273b596cbe58489cc78d8b777a7
DANTE VERDI GUTIERREZ <47573312@ort>
2023-04-19 09:05:24 -0300

    obtenerestadisticas no terminado

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ while (chau) {
         NuevaInscripcion(ref clientes);
         break;
         case ("2"):
-            
+        ObtenerEstadisticas(clientes);
+
         break;
         case ("3"):
             
@@ -41,7 +42,7 @@ void NuevaInscripcion(ref Dictionary<int,Cliente> c){
     apellido=IngresarString("Ingrese Apellido: ");
     nombre=IngresarString("Ingrese Nombre: ");
     while (v){v= DateTime.TryParse(IngresarString("Ingrese la fecha de inscripcion: "), out fecha);}
-    v=true;while (v) {te=IngresarInt("Ingrese el Tipo de Entrada: "); v=te<1&&te>4;};
+    v=true;while (v) {te=IngresarInt("Ingrese el Tipo de Entrada. /n Dia 1:$15000 Dia 2:$30000 Dia 3:$10000 Full Pass (4):$40000 : "); v=te<1&&te>4;};
     switch(te) {
         case 1: va=15000; break;case 2: va=30000; break;case 3: va=10000; break;case 4: va=40000; break;
         }
@@ -49,6 +50,33 @@ void NuevaInscripcion(ref Dictionary<int,Cliente> c){
     c.Add(id,new Cliente(dni,apellido,nombre, fecha, te,va));
 }
 
+void ObtenerEstadisticas(Dictionary<int,Cliente> clientes) {
+
+if (clientes.Any())
+int cantClientes = clientes.Count(); int[] sumaTiposEntrad = new int[4]; Dictionary<DateTime, double> recaudacionPorDia = new Dictionary<DateTime, double>(); double rectotal=0;
+
+foreach(var i in clientes) {
+
+    sumaTiposEntrad[i.Value.TipoEntrada-1]+=1;
+    if (recaudacionPorDia.Keys.Contains(i.Value.FechaInscripcion)) recaudacionPorDia[i.Value.FechaInscripcion] += i.Value.TotalAbonado; 
+    else recaudacionPorDia.Add(i.Value.FechaInscripcion, i.Value.TotalAbonado);
+
+    rectotal+=i.Value.TotalAbonado;
+
+}
+
+//uso el mismo array para la suma y el porcentaje y al que no le gusta que se joda perdon por las malas palabras
+sumaTiposEntrad[0]=sumaTiposEntrad[0]*100/cantClientes;sumaTiposEntrad[1]=sumaTiposEntrad[1]*100/cantClientes;sumaTiposEntrad[2]=sumaTiposEntrad[2]*100/cantClientes;sumaTiposEntrad[3]=sumaTiposEntrad[3]*100/cantClientes;
+
+Console.WriteLine($"Cantidad de clientes: {cantClientes}. \nPorcentajes de cada tipo de entrada: Tipo 1: {sumaTiposEntrad[0]} Tipo 2: {sumaTiposEntrad[2]} Tipo 3: {sumaTiposEntrad[2]} Tipo 4: {sumaTiposEntrad[3]} \nRecaudacion Total: {rectotal}");
+
+Console.WriteLine("Recaudacion de cada dia:");
+foreach(var i in recaudacionPorDia) {
+Console.WriteLine($"Recaudacion del dia {i.Key.ToShortDateString()}: {i.Value}");
+}
+}
+
+#region funcionesingresar
 string IngresarString(string prompt) {
 
     Console.Write(prompt);
@@ -67,3 +95,4 @@ double IngresarDouble(string prompt) {
     while (!b) {b=double.TryParse(Console.ReadLine(), out r);};
     return r;
 }
+#endregion
